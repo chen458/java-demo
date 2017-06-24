f729d42b0acb55aa051b557b1713de8ca615d0d0
陈盛红 <chenshenghong@cheyipai.com>
2017-06-24 16:06:22 +0800

    spring aop

---
+++ src/main/java/com/csh/demo/spring/advice/CountingBeforeAdvice.java
@@ -0,0 +1,17 @@
+package com.csh.demo.spring.advice;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author: shenghong.chen
+ * Date: 2017/6/22
+ * time: 下午11:51
+ */
+public class CountingBeforeAdvice extends MethodCounter implements MethodBeforeAdvice {
+    @Override
+    public void before(Method method, Object[] args, Object target) throws Throwable {
+        count(method);
+    }
+}
---
+++ src/main/java/com/csh/demo/spring/advice/MethodCounter.java
@@ -0,0 +1,29 @@
+package com.csh.demo.spring.advice;
+
+import java.io.Serializable;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+/**
+ * @author: shenghong.chen
+ * Date: 2017/6/22
+ * time: 下午11:38
+ */
+public class MethodCounter implements Serializable {
+    //存储 <方法名, 调用次数>
+    private HashMap<String, Integer> map = new HashMap<>();
+    //所有方法调用总数
+    private int allCount;
+
+    protected void count(Method m) {
+
+    }
+
+    private void count(String methodName) {
+        //线程不安全
+        Integer n = map.get(methodName);
+        n = null != n ? (n.intValue() + 1) : 1;
+        map.put(methodName, n);
+        ++ allCount;
+    }
+}
